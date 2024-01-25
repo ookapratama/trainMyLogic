bbfe93405c21ea752c7106d70dd80a6d95abe322
ookapratama <ookapratama2@gmail.com>
2024-01-26 03:29:19 +0800

    first

---
+++ 1-EvenOrOdd.js
@@ -0,0 +1,11 @@
+// 1 Even and Odd Challenge
+// source : https://www.codewars.com/kata/53da3dbb4a5168369a0000fe/train/javascript
+
+// function evenOrOdd(number) {
+//   if (number % 2 === 0) return "Even";
+//   return "Odd";
+// }
+
+const evenOrOdd = (number) => (number % 2 === 0 ? "Even" : "Odd");
+
+console.log(evenOrOdd(3));
