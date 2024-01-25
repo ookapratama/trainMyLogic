f5ce9d7afb663676451592f149919c4b529f41b7
ookapratama <ookapratama2@gmail.com>
2024-01-26 04:59:57 +0800

    fifth

---
+++ 4-ReturnNegative.js
@@ -0,0 +1,10 @@
+// 4 Return Negative
+// Source : https://www.codewars.com/kata/55685cd7ad70877c23000102/train/javascript
+
+// function makeNegative(num) {
+//   return num > 0 ? -num : num;
+// }
+
+const makeNegative = num => num > 0 ? -num : num;
+
+console.log(makeNegative(5));
