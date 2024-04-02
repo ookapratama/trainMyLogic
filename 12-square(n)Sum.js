326ca88b4ea4d5662be00167b6ffe33c61a641bf
ookapratama <ookapratama2@gmail.com>
2024-04-02 17:32:59 +0800

    twelfth

---
+++ 12-square(n)Sum.js
@@ -0,0 +1,11 @@
+// 12 Square(n) Sum
+// source : https://www.codewars.com/kata/515e271a311df0350d00000f/train/javascript
+
+
+// function squareSum(numbers){
+//   return numbers.reduce((total, value) => (value**2 + total),0)
+// }
+
+const squareSum = numbers => numbers.reduce((total, value) => (value**2 + total), 0)
+
+console.log(squareSum([1]))
