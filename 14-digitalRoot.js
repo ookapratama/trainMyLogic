31af8346fc87a84948a6bceb6d3f5a9ccac7c5bc
ookapratama <ookapratama2@gmail.com>
2024-11-25 07:00:17 +0800

    fourth another solution from forum

--- 14-digitalRoot.js
+++ 14-digitalRoot.js
@@ -2,15 +2,19 @@
 // source : https://www.codewars.com/kata/541c8630095125aba6000c00/train/javascript
 
 function digitalRoot(n) {
-    // console.log(n);
-    // let v = n.toString().split("");
-    // let mapV = v.map((i) => parseInt(i));
-    // let reduceV = mapV.reduce((acc, current) => acc + current, 0);
-    // return reduceV >= 10 ? digitalRoot(reduceV) : reduceV;
+    console.log(n);
+    let v = n.toString().split("");
+    let mapV = v.map((i) => parseInt(i));
+    let reduceV = mapV.reduce((acc, current) => acc + current, 0);
+    return reduceV >= 10 ? digitalRoot(reduceV) : reduceV;
+
     let reduce = n.toString().split("").map((i) => parseInt(i)).reduce((acc, current) => acc + current, 0)
     return n >= 10 ? digitalRoot(reduce)
         : reduce
 
+    // test from forum
+    return (n - 1) % 9 + 1;
+
 }
 
-console.log(digitalRoot(1234567))
+console.log(digitalRoot(12388458))
